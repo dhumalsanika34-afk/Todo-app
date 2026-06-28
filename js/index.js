3cd27e32b333a9c0448cc2c9dace09dd2c0af05e
dhumal sanika <dhumalsanika34@gmail.com>
2026-06-28 21:17:49 +0530

    added the for loop in loadTasks().

--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
 const tasks = [
 
 ];
+console.log(tasks);
 
 function loadTasks() {
-    localStorage.setItem("allTasks", JSON.stringify(tasks ));
+    localStorage.setItem("allTasks", JSON.stringify(tasks));
 
-    const taskContainer = document.getElementById('task-container')
+    const taskContainer = document.getElementById('task-container');
+    taskContainer.innerHTML = '';
+
+    console.log(tasks);
+    for (let i = 0; i < tasks.length; i++) {
+        console.log(tasks[i]);
+        taskContainer.innerHTML += `<div>${tasks[i]}</div>`;
+    }
 }
 
 function addTask() {
